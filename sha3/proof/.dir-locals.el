70c4fcc7fab7beeb17b655625f8096972b86e273
François Dupressoir <fdupress@gmail.com>
2016-02-04 17:19:51 +0100

    Remove old and unused files.
    the  folder may have contained useful stuff. Check history if needed.

--- sha3/proof/.dir-locals.el
+++ sha3/proof/.dir-locals.el
@@ -1,4 +1,4 @@
 ((easycrypt-mode .
   ((eval .
     (flet ((pre (s) (concat (locate-dominating-file buffer-file-name ".dir-locals.el") s)))
-           (setq easycrypt-load-path `(,(pre ".") ,(pre "variant") ,(pre "core"))))))))
+           (setq easycrypt-load-path `(,(pre ".") ,(pre "core"))))))))
