d3dd57d29612abe2f505031c9ef7d6df71d18980
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-05-23 16:42:56 +0200

    fix include paths

--- sha3/proof/.dir-locals.el
+++ sha3/proof/.dir-locals.el
@@ -1,4 +1,4 @@
 ((easycrypt-mode .
   ((eval .
     (flet ((pre (s) (concat (locate-dominating-file buffer-file-name ".dir-locals.el") s)))
-           (setq easycrypt-load-path `(,(pre ".") ,(pre "core") ,(pre "smart_counter"))))))))
+           (setq easycrypt-load-path `(,(pre "smart_counter"))))))))
